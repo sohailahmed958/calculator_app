33f69db4249fe322d8133f3d1c2f42cf14d145d1
Sohail Ahmed <sohailahmedbright958@gmail.com>
2023-08-26 21:47:50 +0500

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+const Color grayColor = Colors.grey;
+const Color greenColor = Colors.green;
+const Color redColor = Colors.red;
+const Color amberColor = Colors.amber;
+const headingTextStyle = TextStyle(fontSize: 30,color: amberColor,fontWeight: FontWeight.w800);
